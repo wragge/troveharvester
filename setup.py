8893d58a4895e519c95a8a700d0d79afe3418879
wragge <tim@discontents.com.au>
2016-04-23 21:35:20 +1000

    More docs and licence

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='troveharvester',
       version='0.1.0',
       packages=['troveharvester'],
-      description='Harvester for Trove newspaper articles.'
-      long_description='This is a tool for harvesting large collections of digitised newspaper articles from Trove (http://trove.nla.gov.au).'
+      description='Tool for harvesting Trove digitised newspaper articles.',
+      long_description=long_description,
+      author='Tim Sherratt',
+      author_email='tim@discontents.com.au',
+      licence='CC0',
+      url='https://github.com/wragge/troveharvester',
       entry_points={
           'console_scripts': [
               'troveharvester = troveharvester.__main__:main'
